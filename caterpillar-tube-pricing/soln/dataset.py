45aeafffd9def1bcf28c32ddb9692866205da141
Constantin Berzan <cberzan@gmail.com>
2015-08-18 13:51:36 -0700

    new approach to augmenting dataset without duplicating work for xv

--- caterpillar-tube-pricing/soln/dataset.py
+++ caterpillar-tube-pricing/soln/dataset.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from datetime import datetime
 from sklearn.cross_validation import StratifiedKFold
 import numpy as np
 import os
@@ -21,16 +22,169 @@ def load_raw_data():
     return raw
 
 
-def generate_xv_splits(raw):
-    """
-    Stratified 10-fold cross-validation iterator.
+def log_transform_y(y):
+    return np.log(y + 1)
 
-    Yields (X_train, y_train, X_test, y_test) tuples of DataFrames.
+
+def inverse_log_transform_y(y):
+    return np.exp(y) - 1
+
+
+def get_specs_df(raw):
     """
-    # Collect the list of unique `tube_assembly_id`s, shuffle them, and split
-    # them into 90% train / 10% test, stratifying on supplier. If the same
-    # tube_assembly_id has multiple suppliers, one is picked arbitrarily.
-    taids = raw['train_set'][['tube_assembly_id', 'supplier']].copy()
+    Return DataFrame with columns `tube_assembly_id` and `specs`.
+
+    Based on specs.csv. Each value in the `specs` column is a list of strings.
+    If a spec occurs more than once for a tube_assembly_id, it will be
+    repeated.
+    """
+    specs_df = pd.DataFrame()
+    specs_df['tube_assembly_id'] = raw['specs']['tube_assembly_id']
+    tmp_df = raw['specs'].where(pd.notnull(raw['specs']), None)
+    specs = [filter(None, row[1:]) for row in tmp_df.values]
+    specs_df['specs'] = specs
+    return specs_df
+
+
+def get_components_df(raw):
+    """
+    Return DataFrame with columns `tube_assembly_id` and `components`.
+
+    Based on bill_of_materials.csv. Each value in the `components` column is a
+    list of strings. If a component occurs with quantity N for a
+    tube_assembly_id, it will be repeated N times in the list. This is OK,
+    because the maximum N is 6 for our dataset.
+    """
+    bill = raw['bill_of_materials']
+    components_df = pd.DataFrame()
+    components_df['tube_assembly_id'] = bill['tube_assembly_id']
+    tmp_df = bill.where(pd.notnull(bill), None)
+    components = []
+    for orig_row in (filter(None, row[1:]) for row in tmp_df.values):
+        new_row = []
+        for component_str, count in zip(orig_row[0::2], orig_row[1::2]):
+            assert int(count) == count
+            new_row.extend([component_str] * int(count))
+        components.append(new_row)
+    components_df['components'] = components
+    return components_df
+
+
+def get_quote_age_feature(dataset):
+    """
+    Return quote_age (quote_date_days_since_1900) Series.
+    """
+    series = pd.to_datetime(dataset['quote_date']) - datetime(1900, 1, 1)
+    return series.astype('timedelta64[D]')
+
+
+def get_adj_quantity_feature(dataset):
+    """
+    Return adj_quantity feature (combining min_order_quantity and quantity).
+    """
+    return dataset[['min_order_quantity', 'quantity']].max(axis=1)
+
+
+def get_adj_bracketing_feature(dataset):
+    """
+    Return adj_bracketing feature (whether there really is bracket pricing).
+    """
+    adj_bracketing = np.zeros(len(dataset), dtype=np.bool)
+    grouped = dataset.groupby(
+        ['tube_assembly_id', 'supplier', 'quote_date'])
+    for t_s_q, indices in grouped.groups.iteritems():
+        if len(indices) > 1:
+            adj_bracketing[indices] = True
+    return adj_bracketing
+
+
+def get_bracketing_pattern_feature(dataset):
+    """
+    Return bracketing_pattern feature (sorted tuple of adj_quantities seen).
+    """
+    grouped = dataset.groupby(
+        ['tube_assembly_id', 'supplier', 'quote_date'])
+    bracketing_pattern = [None] * len(dataset)
+    for t_s_q, indices in grouped.groups.iteritems():
+        if len(indices) > 1:
+            bracket = tuple(sorted(dataset.adj_quantity[indices].values))
+        else:
+            bracket = ()
+        for index in indices:
+            bracketing_pattern[index] = bracket
+    return bracketing_pattern
+
+
+def get_augmented_dataset(orig_set, tube_df, specs_df, components_df):
+    """
+    Return aug_set with the same rows as orig_set, but more features.
+    """
+    aug_set = orig_set
+
+    # Log-transform cost.
+    if 'cost' in aug_set.columns:
+        cost = aug_set.pop('cost')
+        aug_set['log_cost'] = log_transform_y(cost)
+
+    # Join with tube, specs, bill_of_materials tables.
+    aug_set = pd.merge(aug_set, tube_df, on='tube_assembly_id')
+    aug_set = pd.merge(aug_set, specs_df, on='tube_assembly_id')
+    aug_set = pd.merge(aug_set, components_df, on='tube_assembly_id')
+
+    # Rename some features.
+    renamed_cols = {
+        'wall': 'wall_thickness',
+        'other': 'num_other',
+    }
+    aug_set.rename(columns=renamed_cols, inplace=True)
+
+    # Convert some binary features from str to bool.
+    bin_cols = {
+        'bracket_pricing': 'Yes',
+        'end_a_1x': 'Y',
+        'end_a_2x': 'Y',
+        'end_x_1x': 'Y',
+        'end_x_2x': 'Y',
+    }
+    for col, true_val in bin_cols.iteritems():
+        aug_set[col] = (aug_set[col] == true_val)
+
+    # Add computed features.
+    aug_set['quote_age'] = get_quote_age_feature(aug_set)
+    aug_set['adj_quantity'] = get_adj_quantity_feature(aug_set)
+    aug_set['adj_bracketing'] = get_adj_bracketing_feature(aug_set)
+    aug_set['bracketing_pattern'] = get_bracketing_pattern_feature(aug_set)
+
+    # TODO:
+    # - bend_radius from tube.csv has missing values (9999) for 8 rows;
+    #   currently that gets treated as the scalar 9999, which is wrong.
+    # - material_id from tube.csv has missing values; currently
+    #   OneHotFeaturizer treats missing values as a value `nan` that is
+    #   different from 'other' and all the other values. Should we just use
+    #   'other' for missing values?
+    # - end_a and end_x from tube_csv have missing value 'NONE' and '9999',
+    #   which pandas by default treats as two different string values)
+    # - handle duplicate specs (e.g. SP-0007); currently ListFeaturizer
+    #   just treats them as a single one.
+    # - add an `ends` list-valued feature, e.g. [EF-003, EF-006], so that
+    #   the info about both ends is merged into a single feature, which
+    #   gets converted to num_EF_003, num_EF_006, etc. numerical features.
+    # - similarly, add end_1x_count and end_2x count features, treating the
+    #   two ends as interchangeable.
+    # - features like num_sleeve, etc. based on component types
+
+    return aug_set
+
+
+def add_dev_fold_column(aug_train_set, num_folds=10):
+    """
+    Return aug_train_set with added `dev_fold` column.'
+
+    Splits the dataset into `num_folds` folds, stratifying by supplier.
+    """
+    # Collect the list of unique `tube_assembly_id`s, and shuffle them. If the
+    # same tube_assembly_id has multiple suppliers, pick one arbitrarily.
+    taids = aug_train_set[['tube_assembly_id', 'supplier']].copy()
     taids.drop_duplicates(
         subset='tube_assembly_id', take_last=True, inplace=True)
     taids = taids.reset_index(drop=True)
@@ -38,100 +192,36 @@ def generate_xv_splits(raw):
     taids['pos'] = np.random.permutation(len(taids))
     taids.sort('pos', inplace=True)
     taids.pop('pos')
+
+    # Replace rare suppliers with "other".
     suppliers = taids.pop('supplier').values
     counter = Counter(suppliers)
     for i, val in enumerate(suppliers):
-        if counter[val] < 10:
+        if counter[val] < num_folds:
             suppliers[i] = "other"
 
-    # Stratified split.
-    skf = StratifiedKFold(suppliers, n_folds=10, random_state=666)
-    for train_is, test_is in skf:
-        taids['test_set'] = taids.index.isin(test_is)
+    # Split taids into num_folds folds, stratifying by supplier.
+    skf = StratifiedKFold(suppliers, n_folds=num_folds, random_state=666)
+    taids['dev_fold'] = -1
+    for i, (train_is, test_is) in enumerate(skf):
+        taids.loc[test_is, 'dev_fold'] = i
 
-        # Split Xy_all according to taids['test_set'].
-        Xy_all = raw['train_set'].copy()
-        Xy_all = pd.merge(Xy_all, taids, on='tube_assembly_id')
-        Xy_train = Xy_all[Xy_all['test_set'] == False]
-        Xy_train = Xy_train.reset_index(drop=True)
-        Xy_train.pop('test_set')
-        X_train = Xy_train
-        y_train = X_train.pop('cost')
-        Xy_test = Xy_all[Xy_all['test_set'] == True]
-        Xy_test = Xy_test.reset_index(drop=True)
-        Xy_test.pop('test_set')
-        X_test = Xy_test
-        y_test = X_test.pop('cost')
-
-        yield X_train, y_train, X_test, y_test
+    # Now taids is a DataFrame mapping tube_assembly_id to dev_fold.
+    # Merge it with aug_train_set to add the dev_fold column in the latter.
+    return aug_train_set.merge(taids, on='tube_assembly_id')
 
 
-def generate_xv_splits_np(raw, featurizer):
+def get_augmented_train_and_test_set():
     """
-    Split using `generate_xv_splits`, then featurize and convert to ndarrays.
-
-    Yields (X_train_feats, X_train_np, y_train_np, X_test_np, y_test_np)
-    tuples, where X_train_feats is a DataFrame and the others are ndarrays.
+    Return (aug_train_set, aug_test_set) DataFrames.
     """
-    for X_train, y_train, X_test, y_test in generate_xv_splits(raw):
-        # FIXME: stop duplicating work here.
-        X_train_ext = get_extended_X(X_train, raw)
-        X_test_ext = get_extended_X(X_test, raw)
-        y_train_log = log_transform_y(y_train)
-        y_test_log = log_transform_y(y_test)
-
-        featurizer.fit(X_train_ext)
-        X_train_feats = featurizer.transform(X_train_ext)
-        X_test_feats = featurizer.transform(X_test_ext)
-
-        X_train_np = X_train_feats.astype(np.float).values
-        X_test_np = X_test_feats.astype(np.float).values
-        y_train_np = y_train_log.values
-        y_test_np = y_test_log.values
-
-        yield X_train_feats, X_train_np, y_train_np, X_test_np, y_test_np
-
-
-def get_extended_X(X, raw):
-    """
-    Return extended dataset by joining with other files.
-    """
-    X_ext = X
-
-    # Join X_ext with tube.csv by tube_assembly_id.
-    X_ext = pd.merge(X_ext, raw['tube'], on='tube_assembly_id')
-
-    # Convert specs.csv to a DataFrame with columns `tube_assembly_id` and
-    # `specs`, where `specs` is a list of strings.
-    specs_df = pd.DataFrame()
-    specs_df['tube_assembly_id'] = raw['specs']['tube_assembly_id']
-    tmp_df = raw['specs'].where(pd.notnull(raw['specs']), None)
-    specs = [filter(None, row[1:]) for row in tmp_df.values]
-    specs_df['specs'] = specs
-
-    # Join X_ext with specs_df on tube_assembly_id.
-    X_ext = pd.merge(X_ext, specs_df, on='tube_assembly_id')
-
-    # Convert bill_of_materials.csv to a DataFrame with columns
-    # `tube_assembly_id` and `components`, where `components` is a list of
-    # (component_str, quantity) tuples.
-    bill = raw['bill_of_materials']
-    components_df = pd.DataFrame()
-    components_df['tube_assembly_id'] = bill['tube_assembly_id']
-    tmp_df = bill.where(pd.notnull(bill), None)
-    rows = [filter(None, row[1:]) for row in tmp_df.values]
-    components = [list(zip(row[0::2], row[1::2])) for row in rows]
-    components_df['components'] = components
-
-    # Join X_ext with components_df on tube_assembly_id.
-    X_ext = pd.merge(X_ext, components_df, on='tube_assembly_id')
-
-    return X_ext
-
-
-def log_transform_y(y):
-    return np.log(y + 1)
-
-
-def inverse_log_transform_y(y):
-    return np.exp(y) - 1
+    raw = load_raw_data()
+    tube_df = raw['tube']
+    specs_df = get_specs_df(raw)
+    components_df = get_components_df(raw)
+    aug_train_set = get_augmented_dataset(
+        raw['train_set'], tube_df, specs_df, components_df)
+    aug_train_set = add_dev_fold_column(aug_train_set)
+    aug_test_set = get_augmented_dataset(
+        raw['train_set'], tube_df, specs_df, components_df)
+    return aug_train_set, aug_test_set
