72c62f9c7f043a316ac3f6935107f75a38bccc4c
Constantin Berzan <cberzan@gmail.com>
2015-08-30 16:05:32 -0700

    update predict_on_test to use experts

--- caterpillar-tube-pricing/soln/predict_on_test.py
+++ caterpillar-tube-pricing/soln/predict_on_test.py
@@ -3,36 +3,11 @@ from time import time
 import numpy as np
 import pandas as pd
 
-from soln.dataset import AllCategoricalsFeaturizer
 from soln.dataset import get_augmented_train_and_test_set
 from soln.dataset import inverse_log_transform_y
-from soln.layers import layer1_get_indices
-from soln.layers import layer2_get_indices
-from soln.utils import eval_model
-from soln.utils import get_predictions
-from soln.utils import load_model_from_disk
-from soln.utils import train_model
-
-
-def train_or_load_layer(
-        params, get_indices, featurizer, X_train, y_train, filename,
-        from_disk):
-    timer = time()
-
-    if from_disk:
-        print "Loading {} from disk...".format(filename)
-        # This still involves fitting the featurizer...
-        layer = load_model_from_disk(
-            params, get_indices, featurizer, X_train, y_train, filename)
-    else:
-        print "Training {}...".format(filename)
-        layer = train_model(params, get_indices, featurizer, X_train, y_train)
-        layer['model'].save_model(filename)
-
-    timer = time() - timer
-    print "    {} seconds elapsed".format(timer)
-
-    return layer
+from soln.expert_params import base_get_indices
+from soln.experts import get_predictions
+from soln.experts import train_and_save_expert
 
 
 if __name__ == "__main__":
@@ -40,75 +15,29 @@ if __name__ == "__main__":
     timer = time()
     aug_train_set, aug_test_set = get_augmented_train_and_test_set()
     aug_train_set.pop('dev_fold')
-    X_train = aug_train_set
-    y_train = X_train.pop('log_cost')
-    X_test = aug_test_set
     timer = time() - timer
     print "    {} seconds elapsed".format(timer)
 
-    layer1_params = {
-        'objective': 'reg:linear',
-        'silent': 1,
-        'num_rounds': 10000,
-        'gamma': 0.0,
+    print "Training experts..."
+    timer = time()
+    expert_names = ['base', 'supplier41', 'supplier72', 'supplier54']
+    for expert_name in expert_names:
+        train_and_save_expert(expert_name, aug_train_set, folds=False)
+    timer = time() - timer
+    print "    {} seconds elapsed".format(timer)
 
-        'eta': 0.02,
-        'min_child_weight': 6,
-        'max_depth': 8,
-        'subsample': 0.7,
-        'colsample_bytree': 0.6,
-    }
-    layer1_featurizer = AllCategoricalsFeaturizer()
-    layer1 = train_or_load_layer(
-        layer1_params, layer1_get_indices, layer1_featurizer,
-        X_train, y_train, 'layer1.model', from_disk=False)
-
-    layer2_params = {
-        'objective': 'reg:linear',
-        'silent': 1,
-        'num_rounds': 10000,
-        'gamma': 0.0,
-
-        'eta': 0.02,
-        'min_child_weight': 6,
-        'max_depth': 8,
-        'subsample': 0.7,
-        'colsample_bytree': 0.6,
-    }
-    layer2_featurizer = AllCategoricalsFeaturizer()
-    layer2 = train_or_load_layer(
-        layer2_params, layer2_get_indices, layer2_featurizer,
-        X_train, y_train, 'layer2.model', from_disk=False)
-
-    print "Results on train set:"
-    results_1_on_1 = eval_model(
-        layer1['model'], layer1_get_indices, layer1_featurizer,
-        X_train, y_train)
-    print "Layer 1 on all indices: RMSLE {}".format(
-        results_1_on_1['rmsle'])
-    results_1_on_2 = eval_model(
-        layer1['model'], layer2_get_indices, layer1_featurizer,
-        X_train, y_train)
-    print "Layer 1 on layer 2 indices: RMSLE {}".format(
-        results_1_on_2['rmsle'])
-    results_2_on_2 = eval_model(
-        layer2['model'], layer2_get_indices, layer2_featurizer,
-        X_train, y_train)
-    print "Layer 2 on layer 2 indices: RMSLE {}".format(
-        results_2_on_2['rmsle'])
-    y_train_pred = pd.Series(results_1_on_1['y_eval_pred'], copy=True)
-    y_train_pred[results_2_on_2['eval_is']] = results_2_on_2['y_eval_pred']
-    rmsle = np.sqrt(mean_squared_error(y_train.values, y_train_pred.values))
-    print "Layer 1+2 on all indices: RMSLE {}".format(rmsle)
+    X_train = aug_train_set
+    y_train = X_train.pop('log_cost')
+    X_test = aug_test_set
 
     print "Predicting..."
     timer = time()
-    layer1_pred = get_predictions(
-        layer1['model'], layer1_get_indices, layer1_featurizer, X_test)
-    layer2_pred = get_predictions(
-        layer2['model'], layer2_get_indices, layer2_featurizer, X_test)
-    y_test_pred = pd.Series(layer1_pred['y_eval_pred'], copy=True)
-    y_test_pred[layer2_pred['eval_is']] = layer2_pred['y_eval_pred']
+    y_train_pred = get_predictions(
+        'all', expert_names, base_get_indices, aug_train_set)
+    train_rmsle = np.sqrt(mean_squared_error(y_train.values, y_train_pred))
+    print "train RMSLE", train_rmsle
+    y_test_pred = get_predictions(
+        'all', expert_names, base_get_indices, aug_test_set)
     timer = time() - timer
     print "    {} seconds elapsed".format(timer)
 
