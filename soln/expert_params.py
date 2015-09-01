d0872208798ea008607744a042bf6126c85fd2d8
Constantin Berzan <cberzan@gmail.com>
2015-09-01 14:52:47 -0700

    minor cleanup

--- soln/expert_params.py
+++ soln/expert_params.py
@@ -226,3 +226,23 @@ uncommon_suppliers_2_params = {
     'subsample': 0.7,
     'colsample_bytree': 0.6,
 }
+
+
+## Obsolete:
+
+
+def layer1_get_indices(X):
+    return np.ones(len(X), dtype=bool)
+
+
+common_brackets = [
+    (1, 2, 5, 10, 25, 50, 100, 250),
+    (1, 6, 20),
+    (1, 2, 3, 5, 10, 20),
+    (1, 2, 5, 10, 25, 50, 100),
+    (5, 19, 20),
+]
+
+
+def layer2_get_indices(X):
+    return ~X.bracketing_pattern.isin(common_brackets)
--- soln/experts.py
+++ soln/experts.py
@@ -120,23 +120,3 @@ def get_predictions(fold_id, expert_names, get_indices, X_eval):
 
     assert have_y_eval_pred.all()
     return y_eval_pred.values
-
-
-## Obsolete:
-
-
-def layer1_get_indices(X):
-    return np.ones(len(X), dtype=bool)
-
-
-common_brackets = [
-    (1, 2, 5, 10, 25, 50, 100, 250),
-    (1, 6, 20),
-    (1, 2, 3, 5, 10, 20),
-    (1, 2, 5, 10, 25, 50, 100),
-    (5, 19, 20),
-]
-
-
-def layer2_get_indices(X):
-    return ~X.bracketing_pattern.isin(common_brackets)
